cc70f1c136a94480dcb8b5fb414a45370be45a9d
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-02-23 13:56:22 +0100

    feat: crear dos clases nuevas

--- src/olivanders.py
+++ src/olivanders.py
@@ -1,8 +1,17 @@
-class Producto:
-    def __init__(self, nombre: str, sell_in: int, calidad: int):
-        self.nombre = nombre
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
         self.sell_in = sell_in
-        self.calidad = calidad
+        self.quality = quality
 
-    def actualizar(self):
-        raise NotImplementedError
+
+class Inventario:
+
+    def __init__(self, items):
+        self.items = items
+
+    def update(self):
+        for item in self.items:
+            if item.quality > 0:
+                item.quality -= 1
+            item.sell_in -= 1
