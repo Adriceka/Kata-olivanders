1d5c264848002db156c50d5106babe3ba2b59cf7
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-02-25 13:44:40 +0100

    feat: Implementar una acción basica del Item normal

--- src/olivanders.py
+++ src/olivanders.py
@@ -12,6 +12,13 @@ class Inventario:
 
     def update(self):
         for item in self.items:
+
+            if item.sell_in > 0:
+                decremento = 1
+            else:
+                decremento = 2
+
             if item.quality > 0:
-                item.quality -= 1
+                item.quality = max(0, item.quality - decremento)
+
             item.sell_in -= 1
