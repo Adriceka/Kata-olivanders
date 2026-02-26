d906b46dd3ff73fb005ad93e8255d4699e04823d
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-02-26 09:45:52 +0100

    feat: Crear 3 clases nuevas
    Crear clase para sulfuras, backstage y conjured

--- src/olivanders.py
+++ src/olivanders.py
@@ -35,3 +35,34 @@ class AgedBrie(Item):
         incremento = 1 if self.sell_in > 0 else 2
         self.quality += incremento
         self.sell_in -= 1
+
+class Sulfuras(Item):
+
+    def __init__(self, name, sell_in, quality=80):
+        super().__init__(name, sell_in, 80)
+
+    def update(self):
+        pass
+
+
+class Backstage(Item):
+
+    def update(self):
+        if self.sell_in > 10:
+            self.quality += 1
+        elif self.sell_in > 5:
+            self.quality += 2
+        elif self.sell_in > 0:
+            self.quality += 3
+        else:
+            self.quality = 0
+
+        self.sell_in -= 1
+
+
+class Conjured(Item):
+
+    def update(self):
+        decremento = 2 if self.sell_in > 0 else 4
+        self.quality -= decremento
+        self.sell_in -= 1
