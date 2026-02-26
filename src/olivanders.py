ac419e1bc09618795708e1d2db7fa43792313304
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-02-26 09:15:59 +0100

    fix: Mejorar el encapsulamiento
    Mejorar el encapsulamiento de quality con property

--- src/olivanders.py
+++ src/olivanders.py
@@ -2,23 +2,21 @@ class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
-        self.quality = quality
+        self._quality = quality  
+
+    @property
+    def quality(self):
+        return self._quality
+
+    @quality.setter
+    def quality(self, value):
+        self._quality = max(0, min(50, value))
 
 
 class Inventario:
-
     def __init__(self, items):
         self.items = items
 
     def update(self):
         for item in self.items:
-
-            if item.sell_in > 0:
-                decremento = 1
-            else:
-                decremento = 2
-
-            if item.quality > 0:
-                item.quality = max(0, item.quality - decremento)
-
-            item.sell_in -= 1
+            item.update()
